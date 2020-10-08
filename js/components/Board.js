d8ffb4102a999d835d2bf43c377a62aeace345b2
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2020-10-08 10:13:48 +0545

    Feature: 'Add another list' added

--- js/components/Board.js
+++ js/components/Board.js
@@ -8,17 +8,22 @@ export default {
           <div class="card">
             <div class="card-header font-weight-bold pb-2">{{type.name}}</div>
             <div class="card-body px-2 pt-0 pb-0 parent">
-              <div class="card mb-2 child" v-for="(child, index) in type.childs" draggable="true">
+              <div
+                class="card mb-2 child"
+                v-for="(child, index) in type.childs"
+                draggable="true"
+                @click="show(child)"
+              >
                 <div class="card-body py-2">
-                  <!--<span class="badge badge-primary">Minor Bug</span>-->  
+                  <!--<span class="badge badge-primary">Minor Bug</span>-->
                   <p class="mb-0">{{child}}</p>
                 </div>
               </div>
             </div>
-            <div class="card-body p-2 actions" v-if="card.active != type.name">
+            <div class="card-body p-2 actions" v-if="card.active != type.slug">
               <button
                 class="btn btn-light btn-block"
-                @click="card.active = type.name"
+                @click="card.active = type.slug"
               >
                 + Add another card
               </button>
@@ -44,6 +49,30 @@ export default {
         </div>
 
         <!-- -->
+
+        <div class="col px-1">
+          <div class="card add-another-list">
+            <button class="btn btn-dark translucent btn-block" v-if="!addAnotherList.toggle" @click="addAnotherList.toggle = true">+ Add another list</button>
+            <div class="card-body p-2 actions" v-else>
+              <textarea
+                class="form-control mb-2"
+                rows="1"
+                placeholder="Enter list title..."
+                v-model="addAnotherList.value"
+              ></textarea>
+              <button
+                class="btn btn-success"
+                :disabled="addAnotherList.value.length==0"
+                @click="addAnotherListToTypes"
+              >
+                Add Card
+              </button>
+              <span aria-hidden="true" class="close" @click="addAnotherList.toggle = false"
+                >&times;</span
+              >
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `,
@@ -52,6 +81,10 @@ export default {
     return {
       card: {
         active: "",
+        value: "",
+      },
+      addAnotherList: {
+        toggle: false,
         value: "",
       },
       types: [
@@ -124,12 +157,42 @@ export default {
   methods: {
     add(index) {
       this.types[index].childs.push(this.card.value);
+      setTimeout(() => {
+        this.addEventListener();
+      }, 0);
       this.reset();
     },
 
     reset() {
       this.card.value = "";
       this.card.active = "";
+    },
+
+    show(card) {
+      console.log(card);
+    },
+
+    addAnotherListToTypes() {
+      let slug = "";
+      let index = -1;
+      do {
+        index = -1;
+        slug =
+          this.addAnotherList.value.toLocaleLowerCase().replaceAll(" ", "_") +
+          "_" +
+          Math.floor(Math.random() * 99999);
+        index = this.types.findIndex((type) => type.slug == slug);
+      } while (index > 0);
+      this.types.push({
+        name: this.addAnotherList.value,
+        slug,
+        childs: [],
+      });
+      this.addAnotherList.value = "";
+      this.addAnotherList.toggle = false;
+      setTimeout(() => {
+        this.addEventListener();
+      }, 0);
     },
 
     addEventListener() {
